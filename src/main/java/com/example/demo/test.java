06f35ed7537bd2f6e36c68be42446ed41ab0e8b1
linghuchong <linghuchong@163.com>
2022-09-06 10:13:11 +0800

    master commit

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -6,6 +6,7 @@ public class test {
 
         System.out.println("first");
         System.out.println("second");
+        System.out.println("master");
         System.out.println("host-fix ");
     }
 }
